cc72caeb7d239add483022b2c2ad101451788f57
yjmc25 <yalimiranda@gmail.com>
2021-04-30 04:00:49 -0700

    wrote marklar method

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,54 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    i = 0 # index of current character
+    firstLetter = 0 # index of the first letter in the current word
+
+    str.each_char do |char|
+
+      # if the previous character is not a letter and the current character is a letter
+      # then this is the start of the current word
+      if !(/[[:alpha:]]/).match(str[i-1]) && (/[[:alpha:]]/).match(str[i])
+        firstLetter = i
+      end
+
+      # if the next character is not a letter and the current character is a letter
+      # then this is the end of the current word
+      if !(/[[:alpha:]]/).match(str[i+1]) && (/[[:alpha:]]/).match(str[i])
+
+        lastLetter = i # index of the last letter in the current word
+        wordLength = lastLetter - firstLetter + 1 #current word length
+
+        if wordLength > 4
+
+          # check if the first character of the current word is a capital letter
+          if (/[[:upper:]]/).match(str[firstLetter])
+            marklarStr = "Marklar"
+          else
+            marklarStr = "marklar"
+          end
+
+          # replace current word with an empty string
+          str[firstLetter,wordLength] = ""
+          # insert the marklar string at the index where the current word starts
+          str.insert(firstLetter,marklarStr)
+
+          # adjust the index for the difference in length between the current word and marklar
+          if marklarStr.length > wordLength
+            i += marklarStr.length - wordLength
+          elsif marklarStr.length < wordLength
+            i -= wordLength - marklarStr.length
+          end
+          
+        end
+
+      end
+
+      # increment the index by one
+      i += 1
+    end
+
+    return str
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
